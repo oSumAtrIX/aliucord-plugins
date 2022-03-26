b101a69f50008ea7b4d4ac540d2f8a3d258964cb
Manti <67705577+mantikafasi@users.noreply.github.com>
2022-03-26 21:57:53 +0300

    fixcxc

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,11 @@ buildscript {
         google()
         mavenCentral()
         maven("https://jitpack.io")
+        maven("https://maven.aliucord.com/snapshots")
+
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("com.android.tools.build:gradle:7.0.4")
         classpath("com.github.Aliucord:gradle:main-SNAPSHOT")
     }
 }
@@ -17,7 +19,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io")
+        maven("https://maven.aliucord.com/snapshots")
     }
 }
 
@@ -51,14 +53,10 @@ subprojects {
 
     dependencies {
         val discord by configurations
-        val implementation by configurations
+        val compileOnly by configurations
 
         discord("com.discord:discord:aliucord-SNAPSHOT")
-        implementation("com.github.Aliucord:Aliucord:main-SNAPSHOT")
-
-        implementation("androidx.appcompat:appcompat:1.3.1")
-        implementation("com.google.android.material:material:1.4.0")
-        implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+        compileOnly("com.aliucord:Aliucord:main-SNAPSHOT")
     }
 }
 
